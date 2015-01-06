9ff917f291c3fcc24682ef952ea54aff98c5ba12
Felix Ebert <felix.ebert@if-core.de>
2015-01-06 17:06:55 +0100

    data update

--- src/js/map.js
+++ src/js/map.js
@@ -2,9 +2,12 @@
     'use strict';
 
     var leafletMap = null;
-    var areas = [];
     var colorScheme = ["#fff5f0", "#fee0d2", "#fcbba1", "#fc9272", "#fb6a4a", "#ef3b2c", "#cb181d", "#a50f15", "#67000d"];
     var info = [];
+    var metaData = {
+        timerangeId: null,
+        label: ''
+    };
 
     var formatNumber = function (n) {
         var rx = /(\d+)(\d{3})/;
@@ -24,6 +27,9 @@
             maxZoom: 16
         });
 
+        metaData.timerangeId = '14-10_14-12';
+        metaData.label = 'Polizeiberichte Okt. - Dez. 2014';
+
         addTileLayer();
         var markersInstance = new cm.map.markers(leafletMap);
         cm.data.addListener(function () {
@@ -33,12 +39,13 @@
             updateAreaLayers(cm.data.areas);
         });
         createInfoControl();
+        createLegendControl();
 
-        cm.data.update('13-11_14-04', null, 8);
+        cm.data.update(metaData.timerangeId, null, 8);
     };
 
     var addTileLayer = function () {
-        var attribution = '<a href="http://www.openstreetmap.org/copyright">&copy; OpenStreetMap contributors</a> | <a href="http://www.mapbox.com/about/maps/" target="_blank">Mapbox Terms &amp; Feedback</a> | <a href="http://www.stimme.de/meta/ueberuns/impressum/Impressum;art5015,1284151">Impressum</a> | Polizeiberichte Nov. 13 - Apr. 14 | Alle Angaben ohne Gewähr!';
+        var attribution = '<a href="http://www.openstreetmap.org/copyright">&copy; OpenStreetMap contributors</a> | <a href="http://www.stimme.de/meta/ueberuns/impressum/Impressum;art5015,1284151">Impressum</a> | Angaben ohne Gewähr!';
         L.tileLayer('https://{s}.tiles.mapbox.com/v3/codeforheilbronn.i4f96icg/{z}/{x}/{y}.png', {
             'maxZoom': 18,
             'attribution': attribution
@@ -108,6 +115,27 @@
         });
 
         info.addTo(leafletMap);
+    };
+
+    var createLegendControl = function () {
+        var legend = L.control();
+
+        legend.onAdd = function () {
+            this._div = L.DomUtil.create('div', 'legend');
+            this.update();
+            return this._div;
+        };
+
+        legend.update = function () {
+            var html = '';
+
+            html += '<h4>Polizeiberichte ' + metaData.label + '</h4>';
+
+            this._div.innerHTML = html;
+        };
+
+        legend.addTo(leafletMap);
+        return legend;
     };
 
     var updateAreaLayers = function (areas) {
